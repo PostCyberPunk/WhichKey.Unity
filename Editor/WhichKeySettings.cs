a6d7dd287a3dfe33a7c3815bf64b8ceef490d98d
PostCyberPunk <lyhguest2@outlook.com>
2023-08-25 05:50:46 +0800

    feat process key seq init

--- Editor/WhichKeySettings.cs
+++ Editor/WhichKeySettings.cs
@@ -1,23 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEditor;
 using UnityEditorInternal;
 using UnityEngine;
 namespace PCP.Tools.WhichKey
 {
+	//
 	[FilePath("Preferences/WhichKeySettings.asset", FilePathAttribute.Location.PreferencesFolder)]
 	public class WhichKeySettings : ScriptableSingleton<WhichKeySettings>
 	{
-		public List<KeySet> keySets = new List<KeySet>();
-		public bool ShowHint;
-		public float HintDelayTime;
-		private void Awake()
+		[SerializeField] public List<KeySet> keySets = new List<KeySet>();
+		[SerializeField] public bool ShowHint;
+		[SerializeField] public float HintDelayTime;
+		private Dictionary<int, KeySet> mKeySetDict;
+		private StringBuilder mKeySeq;
+		private void Init()
 		{
-			// WhichKey.mSettings = this;
-			HintDelayTime = 1;
-			Debug.Log("WhichKeySettings Awake");
+			mKeySetDict = new Dictionary<int, KeySet>();
+			mKeySeq = new StringBuilder();
+
+			foreach (var keySet in keySets)
+			{
+				mKeySetDict.Add(keySet.key.GetHashCode(), keySet);
+			}
+		}
+		public bool ProcessKeySeq(string key)
+		{
+			mKeySeq.Append(key);
+			mKeySeq.Replace("alpha", "");
+			//find key in keyset
+			KeySet keySet;
+			mKeySetDict.TryGetValue(mKeySeq.ToString().GetHashCode(), out keySet);
+			if (keySet == null)
+			{
+				mKeySeq.Clear();
+				return false;
+			}
+			//process key
+			switch (keySet.type)
+			{
+				case KeyCmdType.Layer:
+					ProcessLayer(keySet.CmdArg0);
+					break;
+				case KeyCmdType.Menu:
+					ProcessMenu(keySet.CmdArg0);
+					break;
+				// case KeyCmdType.File:
+				// 	WhichKey.ProcessFile(keySet.CmdArg0);
+				// 	break;
+				// case KeyCmdType.ChangeRoot:
+				// 	WhichKey.ProcessChangeRoot(keySet.CmdArg0);
+				// 	break;
+				default:
+					return false;
+			}
+			return true;
+		}
+		void ProcessLayer(string layerName)
+		{
+			// LayerManager.instance.SetLayer(layerName);
+		}
+		void ProcessMenu(string menuName)
+		{
+			EditorApplication.ExecuteMenuItem(menuName);
+		}
+		public static void ApplySettins()
+		{
+			instance.Init();
 		}
 	}
+
+
 	static class WhichkeySettingProvider
 	{
 		private static ReorderableList mKeySetList;
