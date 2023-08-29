8a4fcc75154dcd326ab882261944a4ad82a2d5f3
PostCyberPunk <lyhguest2@outlook.com>
2023-08-30 03:31:39 +0800

    test!: EditorWindow LifeCycle

--- Editor/WKTestWIndow.cs
+++ Editor/WKTestWIndow.cs
@@ -8,24 +8,41 @@ namespace PCP.Tools.WhichKey
 	public class WKTestWindow : EditorWindow
 	{
 		private Label TestLabel;
-        private float fontSize;
-		public static void Test(float fontSize)
+		private float fontSize;
+        private void Awake()
+        {
+            Debug.Log("Awake");
+        }
+        private void OnEnable()
+        {
+            Debug.Log("OnEnable");
+        }
+		internal static void Test(float fontSize)
 		{
 			WKTestWindow win = ScriptableObject.CreateInstance<WKTestWindow>();
-            win.fontSize = fontSize;
-            win.position = new Rect(0,0,5,50);
+			win.fontSize = fontSize;
+			win.position = new Rect(0, 0, 5, 50);
 			win.ShowPopup();
-		}
-		private void CreateGUI()
-		{
-			TestLabel = new Label("W");
-            TestLabel.style.fontSize = fontSize;
-			rootVisualElement.Add(TestLabel);
 		}
         private void OnGUI()
         {
-            WhichKeyWindow.lineHeight = TestLabel.resolvedStyle.height;
-            Close();
+            Debug.Log("OnGUI");
+            Debug.Log(TestLabel==null);
         }
+		private void CreateGUI()
+		{
+			TestLabel = new Label("W");
+			TestLabel.style.fontSize = fontSize;
+			rootVisualElement.Add(TestLabel);
+            Debug.Log("CreateGUI");
+		}
+
+		private void OnBecameVisible()
+		{
+			// WhichKeyWindow.lineHeight = TestLabel.resolvedStyle.height;
+			Debug.Log("OnBecameVisible");
+			// Close();
+		}
+		private void OnLostFocus() => Close();
 	}
 }
