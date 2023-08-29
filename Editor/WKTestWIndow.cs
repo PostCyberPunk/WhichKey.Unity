c604b0a1ba9a3523f49dd4c7f15afb21cce97e00
PostCyberPunk <lyhguest2@outlook.com>
2023-08-30 03:01:09 +0800

    feat TestWin to calc lineheight

---
+++ Editor/WKTestWIndow.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+namespace PCP.Tools.WhichKey
+{
+	public class WKTestWindow : EditorWindow
+	{
+		private Label TestLabel;
+        private float fontSize;
+		public static void Test(float fontSize)
+		{
+			WKTestWindow win = ScriptableObject.CreateInstance<WKTestWindow>();
+            win.fontSize = fontSize;
+            win.position = new Rect(0,0,5,50);
+			win.ShowPopup();
+		}
+		private void CreateGUI()
+		{
+			TestLabel = new Label("W");
+            TestLabel.style.fontSize = fontSize;
+			rootVisualElement.Add(TestLabel);
+		}
+        private void OnGUI()
+        {
+            WhichKeyWindow.lineHeight = TestLabel.resolvedStyle.height;
+            Close();
+        }
+	}
+}
