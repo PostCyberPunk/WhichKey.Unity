82660d7313420304e7e017978334ca188b0185f1
PostCyberPunk <lyhguest2@outlook.com>
2023-08-30 00:38:24 +0800

    fix iwk namespace

--- Editor/IWhichKeyHandler.cs
+++ Editor/IWhichKeyHandler.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IWhichKeyHandler
+namespace PCP.Tools.WhichKey
 {
-	public bool ProcessKey(char key);
-	public string[] GetLayerHints();
+	public interface IWhichKeyHandler
+	{
+		public bool ProcessKey(char key);
+
+		public string[] GetLayerHints();
+	}
 }
