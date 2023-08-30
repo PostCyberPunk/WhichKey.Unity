e1d48190f94c60b03b539ddc6c4db68b1b29da55
PostCyberPunk <lyhguest2@outlook.com>
2023-08-31 04:18:28 +0800

    feat prjaData init

---
+++ Editor/SO/ProjectAssetsData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+namespace PCP.Tools.WhichKey
+{
+
+    [CreateAssetMenu(fileName = "ProjectAssetsData", menuName = "Whichkey/ProjectAssetsData", order = 1)]
+    public class ProjectAssetsData : ScriptableObject
+    {
+        [SerializeField] private string LayerHints;
+        private void Awake()
+        {
+            LayerHints = "LayerHints";
+        }
+    }
+}
