2c07e2d23e7be1c43854d92ff9dc65503ba32a9f
PostCyberPunk <lyhguest2@outlook.com>
2023-08-31 04:18:38 +0800

    feat prjset init

---
+++ Editor/Settings/WhichkeyProjectSettings.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace PCP.Tools.WhichKey
+{
+    [FilePath("Project/WhichkeyProjectSettings", FilePathAttribute.Location.ProjectFolder)]
+    public class WhichkeyProjectSettings : ScriptableSingleton<WhichkeyProjectSettings>
+    {
+        [SerializeField] public bool showHintInstant = true;
+        [SerializeField] public KeySet[] keySets;
+        [SerializeField] public ProjectAssetsData[] projectAssetsDatas;
+        public static void Save()
+        {
+            Undo.RegisterCompleteObjectUndo(instance, "Save WhichKey Settings");
+            instance.Save(true);
+        }
+        internal SerializedObject GetSerializedObject()
+        {
+            return new SerializedObject(this);
+        }
+        private void OnEnable()
+        {
+            hideFlags &= ~HideFlags.NotEditable;
+        }
+    }
+
+}
