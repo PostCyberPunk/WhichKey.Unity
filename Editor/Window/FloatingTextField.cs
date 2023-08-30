473116bf21fba1f9d1109f6a89cf737158e25b4e
PostCyberPunk <lyhguest2@outlook.com>
2023-08-30 11:50:46 +0800

    feat float inputfield win

---
+++ Editor/Window/FloatingTextField.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace PCP.Tools.WhichKey
+{
+    public class FloatingTextField : EditorWindow
+    {
+        private string mHint;
+        private bool shouldFocus;
+        private static readonly Vector2 mSize = new(200, 22);
+        private TextField textField;
+        private System.Action<string> onInputComplete;
+
+        public static void ShowInputField(System.Action<string> onComplete, string hint = "Enter Here:")
+        {
+            var win = CreateInstance<FloatingTextField>();
+            win.mHint = hint;
+            win.onInputComplete = onComplete;
+            win.shouldFocus = true;
+            win.minSize = mSize;
+            win.maxSize = mSize;
+            win.position = new Rect(0, 0, mSize.x, mSize .y);
+            win.ShowPopup();
+        }
+
+        private void CreateGUI()
+        {
+            textField = new TextField(mHint);
+            rootVisualElement.Add(textField);
+        }
+        private void OnGUI()
+        {
+            if (shouldFocus)
+            {
+                if (Event.current != null)
+                {
+                    Vector2 mousePos = GUIUtility.GUIToScreenPoint(Event.current.mousePosition);
+                    position = new Rect(mousePos.x - mSize.x / 2, mousePos.y - mSize.y / 2, maxSize.x, mSize.y);
+                }
+                shouldFocus = false;
+                textField.Focus();
+                textField.RegisterCallback<KeyDownEvent>(e =>
+                {
+                    if (e.keyCode == KeyCode.Return)
+                    {
+                        onInputComplete?.Invoke(textField.value);
+                        Close();
+                    }
+                });
+            }
+        }
+        private void OnLostFocus() => Close();
+    }
+
+}
