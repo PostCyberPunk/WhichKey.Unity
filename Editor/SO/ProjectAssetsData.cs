5041c6c6aaf227f4ee7f25b24b1cbc4e781d6f7f
PostCyberPunk <lyhguest2@outlook.com>
2023-09-01 23:22:59 +0800

    feat projectAssetsData OnAsssetsChaange

--- Editor/SO/ProjectAssetsData.cs
+++ Editor/SO/ProjectAssetsData.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+using Unity.Properties;
 namespace PCP.Tools.WhichKey
 {
 
     [CreateAssetMenu(fileName = "ProjectAssetsData", menuName = "Whichkey/ProjectAssetsData", order = 1)]
     public class ProjectAssetsData : ScriptableObject
     {
-        [SerializeField] public AssetsData[] Assets = new AssetsData[0];
         [SerializeField] public string[] LayerHints;
-        public string testr = "hello";
+        [SerializeField] private AssetsData[] Assets = new AssetsData[0];
         public string GetAssetsPathByKey(char key)
         {
             foreach (var item in Assets)
@@ -20,6 +21,19 @@ namespace PCP.Tools.WhichKey
                     return item.AssetPath;
             }
             return "";
+        }
+        private void OnValidate()
+        {
+            OnAssetsChange();
+        }
+        private void OnAssetsChange()
+        {
+            LayerHints = new string[Assets.Length * 2];
+            for (int i = 0; i < Assets.Length; i++)
+            {
+                LayerHints[i * 2] = Assets[i].Key.ToString();
+                LayerHints[i * 2 + 1] = Assets[i].Hint;
+            }
         }
     }
     [System.Serializable]
