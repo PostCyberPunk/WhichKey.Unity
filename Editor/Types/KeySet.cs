50522e3d8546417f0ed76c09b3b2064f693c2359
PostCyberPunk <lyhguest2@outlook.com>
2023-09-05 00:18:13 +0800

    feat keyset setKeylabel

--- Editor/Types/KeySet.cs
+++ Editor/Types/KeySet.cs
@@ -5,10 +5,17 @@ namespace PCP.Tools.WhichKey
 	[Serializable]
 	public class KeySet
 	{
-		public int[] KeySeq=new int[0];
+		public int[] KeySeq = new int[0];
 		public string KeyLabel = "None";
 		public KeyCmdType type;
 		public string HintText;
 		public string CmdArg;
+		public void SetKeyLabel()
+		{
+			if (KeySeq.Length == 0)
+				KeyLabel = "None";
+			else
+				KeyLabel = KeySeq.ToLabel();
+		}
 	}
 }
