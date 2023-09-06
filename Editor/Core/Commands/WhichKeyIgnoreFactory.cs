44ff92eb9051cb6ac361d1cc85d233fe7936c26b
PostCyberPunk <lyhguest2@outlook.com>
2023-09-07 05:41:39 +0800

    feat(BaseCommandFactory): ignore factory attibute init

---
+++ Editor/Core/Commands/WhichKeyIgnoreFactory.cs
@@ -0,0 +1,9 @@
+using System;
+namespace PCP.Tools.WhichKey
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+	public class WhichKeyIgnoreFactory : Attribute
+	{
+
+	}
+}
