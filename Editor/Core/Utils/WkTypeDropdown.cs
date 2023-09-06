7aa14e11b900cf0578b5e5cb4bc55c7d530501e5
PostCyberPunk <lyhguest2@outlook.com>
2023-09-06 12:51:59 +0800

    wkTypedropdown

---
+++ Editor/Core/Utils/WkTypeDropdown.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+using System.Linq;
+using System;
+using Microsoft.SqlServer.Server;
+using System.Drawing.Drawing2D;
+
+namespace PCP.Tools.WhichKey
+{
+
+    public class WkTypeDropdown : PopupField<int>
+    {
+        // public new class UxmlFactory : UxmlFactory<WkTypeDropdown> {}
+        private static Dictionary<int, string> mDict => CmdFactoryManager.CommandTypeMap;
+        public new class UxmlFactory :
+            UxmlFactory<WkTypeDropdown, BaseFieldTraits<int, UxmlIntAttributeDescription>>
+        { }
+        public WkTypeDropdown() : base(mDict.Keys.ToList(), 0, Format, Format)
+        {
+            // this.RegisterValueChangedCallback(OnValueChanged);
+        }
+        private static string Format(int i) => mDict[i];
+        // private void OnValueChanged(ChangeEvent<int> evt)
+        // {
+        //     int index = this.index;
+
+        // }
+
+    }
+}
