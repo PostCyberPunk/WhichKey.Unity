827874b55068981ad1f8d1207844a44a5d0004f9
PostCyberPunk <lyhguest2@outlook.com>
2023-09-07 01:18:51 +0800

    fix:dropdown and jsonupdater
    fix(wktypeDropwon): format
    
    fix(JsonUpdater): update
    
    Revert "fix(JsonUpdater): update"
    
    This reverts commit 395a445d948bfa044512ec2a20a05af42490ff63.

--- Editor/Core/Utils/WkTypeDropdown.cs
+++ Editor/Core/Utils/WkTypeDropdown.cs
@@ -22,7 +22,13 @@ namespace PCP.Tools.WhichKey
         {
             // this.RegisterValueChangedCallback(OnValueChanged);
         }
-        private static string Format(int i) => mDict[i];
+        private static string Format(int i)
+        {
+            if (mDict.ContainsKey(i))
+                return mDict[i];
+            else
+                return $"<color=red>Not Found {i.ToString()}</color>";
+        }
         // private void OnValueChanged(ChangeEvent<int> evt)
         // {
         //     int index = this.index;
