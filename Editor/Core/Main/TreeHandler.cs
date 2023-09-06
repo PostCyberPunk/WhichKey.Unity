0696c49a5c0ec3ea07220114bbf35d620395be30
PostCyberPunk <lyhguest2@outlook.com>
2023-09-06 08:05:34 +0800

    treehdl rmv unused dictionary

--- Editor/Core/Main/TreeHandler.cs
+++ Editor/Core/Main/TreeHandler.cs
@@ -6,31 +6,6 @@ namespace PCP.Tools.WhichKey
 {
     public class TreeHandler : IWKHandler
     {
-        private static readonly Dictionary<KeyCode, string> mKeycodeMap = new Dictionary<KeyCode, string>
-        {
-        { KeyCode.Space, " " },
-        { KeyCode.Alpha0, "0" },
-        { KeyCode.Alpha1, "1" },
-        { KeyCode.Alpha2, "2" },
-        { KeyCode.Alpha3, "3" },
-        { KeyCode.Alpha4, "4" },
-        { KeyCode.Alpha5, "5" },
-        { KeyCode.Alpha6, "6" },
-        { KeyCode.Alpha7, "7" },
-        { KeyCode.Alpha8, "8" },
-        { KeyCode.Alpha9, "9" },
-        { KeyCode.Keypad0, "0" },
-        { KeyCode.Keypad1, "1" },
-        { KeyCode.Keypad2, "2" },
-        { KeyCode.Keypad3, "3" },
-        { KeyCode.Keypad4, "4" },
-        { KeyCode.Keypad5, "5" },
-        { KeyCode.Keypad6, "6" },
-        { KeyCode.Keypad7, "7" },
-        { KeyCode.Keypad8, "8" },
-        { KeyCode.Keypad9, "9" },
-        };
-
         private readonly TreeBuilder mTreeBuilder = new TreeBuilder();
         private Stack<int> mKeySeq;
         private KeyNode mTreeRoot => mTreeBuilder.TreeRoot;
