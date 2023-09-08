eabb11ac7daba1421534d1319f5fc312e418abae
PostCyberPunk <lyhguest2@outlook.com>
2023-09-09 01:50:35 +0800

    cmd init

---
+++ Editor/Extra/SceneNav/SceneNavCmd.cs
@@ -0,0 +1,26 @@
+namespace PCP.Tools.WhichKey
+{
+	internal class SceneNavCmd : ChangeHandlerCmd
+	{
+		public override IWKHandler Handler => mSceneHandler;
+		public override bool isEnd => false;
+		public override int Depth => 1;
+		private readonly static SceneHandler mSceneHandler = new();
+		private bool set = false;
+		public SceneNavCmd(bool isSst) => set = isEnd;
+		protected override void ActiveHandler() => mSceneHandler.Set = set;
+	}
+	internal class SceneNavLoadCmdFactory : WKCommandFactory
+
+	{
+		public override int TID => 13;
+		public override string CommandName => "SceneNav";
+		public override WKCommand CreateCommand(string arg) => new SceneNavCmd(false);
+	}
+	internal class SceneNavSetCmdFactory : WKCommandFactory
+	{
+		public override int TID => 14;
+		public override string CommandName => "SceneNavSet";
+		public override WKCommand CreateCommand(string arg) => new SceneNavCmd(true);
+	}
+}
