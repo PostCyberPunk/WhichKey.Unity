ea78bfd91cd052f776b8c4b155e0d0559eeb86c5
PostCyberPunk <lyhguest2@outlook.com>
2023-09-10 03:23:08 +0800

    temp remove

--- Editor/Core/Commands/WhichKeyIgnoreFactory.cs
+++
@@ -1,9 +0,0 @@
-using System;
-namespace PCP.Tools.WhichKey
-{
-	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
-	public class WhichKeyIgnoreFactory : Attribute
-	{
-
-	}
-}
