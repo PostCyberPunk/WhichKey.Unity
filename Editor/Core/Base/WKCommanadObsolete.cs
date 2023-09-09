acd5da06219991ba7ee1767a609a66a9a6c6260f
PostCyberPunk <lyhguest2@outlook.com>
2023-09-10 03:13:09 +0800

    feat(Wk coomand): RemoveFactory

---
+++ Editor/Core/Base/WKCommanadObsolete.cs
@@ -0,0 +1,9 @@
+namespace PCP.Tools.WhichKey
+{
+	public interface WKCommanadObsolete
+	{
+		bool isEnd =>true;
+		void Execute();
+	}
+
+}
--- Editor/Core/Base/WKCommand.cs
+++ Editor/Core/Base/WKCommand.cs
@@ -1,9 +1,15 @@
+using System.Collections.Generic;
+using UnityEditor;
+
 namespace PCP.Tools.WhichKey
 {
-	public interface WKCommand
+	public abstract class WKCommand
 	{
-		bool isEnd =>true;
-		void Execute();
+		public abstract int TID { get; }
+		public abstract string CommandName { get; }
+		public virtual bool isEnd => true;
+		public abstract WKCommand Init();
+		public abstract void Execute(string arg);
 	}
 
 }
