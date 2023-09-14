200641adb800f2599c40b8f53929a62bd558d6f8
PostCyberPunk <lyhguest2@outlook.com>
2023-09-15 01:44:59 +0800

    refactor pref

--- Core/Editor/Settings/WhichKeyPreferences.cs
+++ Core/Editor/Settings/WhichKeyPreferences.cs
@@ -5,14 +5,15 @@ using PCP.WhichKey.Log;
 namespace PCP.WhichKey.Core
 {
 	[FilePath("Preferences/WhichkeyPreferences.asset", FilePathAttribute.Location.PreferencesFolder)]
-	internal class WhichKeyPreferences : WkSettingBase<WhichKeyPreferences>
+	public class WhichKeyPreferences : WkSettingBase<WhichKeyPreferences>
 	{
-		public float Timeout = 1;
+		public float Timeout = 0;
 		public bool WindowFollowMouse = true;
 		public Vector2 FixedPosition;
 		public int MaxHintLines = 10;
 		public float ColWidth = 250;
-		public LoggingLevel LogLevel = LoggingLevel.Info;
+		[SerializeField]
+		internal LoggingLevel LogLevel = LoggingLevel.Info;
 		public bool UseInPlaymode = false;
 	}
 }
