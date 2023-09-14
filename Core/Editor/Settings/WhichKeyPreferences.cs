2bd20995d093ce07d8ac86574d25b0066fcdfcdc
PostCyberPunk <lyhguest2@outlook.com>
2023-09-15 01:41:08 +0800

    fix(SettingProvider): base json method override
    refactor pref
    
    change pref save location

--- Core/Editor/Settings/WhichKeyPreferences.cs
+++ Core/Editor/Settings/WhichKeyPreferences.cs
@@ -4,15 +4,16 @@ using PCP.WhichKey.Log;
 
 namespace PCP.WhichKey.Core
 {
-	[FilePath("Preferences/WhichkeyPreferences.asset", FilePathAttribute.Location.PreferencesFolder)]
-	internal class WhichKeyPreferences : WkSettingBase<WhichKeyPreferences>
+	[FilePath("WhichKey/Preferences.asset", FilePathAttribute.Location.PreferencesFolder)]
+	public class WhichKeyPreferences : WkSettingBase<WhichKeyPreferences>
 	{
-		public float Timeout = 1;
+		public float Timeout = 0;
 		public bool WindowFollowMouse = true;
 		public Vector2 FixedPosition;
 		public int MaxHintLines = 10;
 		public float ColWidth = 250;
-		public LoggingLevel LogLevel = LoggingLevel.Info;
+		[SerializeField]
+		internal LoggingLevel LogLevel = LoggingLevel.Info;
 		public bool UseInPlaymode = false;
 	}
 }
